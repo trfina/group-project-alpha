2515843f3dec3cae3986e97ba657b0f9f7b2b558
Alan Drumm <asdrumm1234@gmail.com>
2021-12-12 22:15:04 -0500

    Working on Free Week Rotation code Still getting an error

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,7 @@
-const API_KEY = "RGAPI-2227c53f-77f9-44df-b194-43551ad47f54";
+const API_KEY = "RGAPI-12fbfd1a-4e1c-4285-994a-fc8bb918a5d8";
 var btnGetAccount = document.querySelector("#getAccount");
 var btnGetFreeWeek = document.querySelector("#getFreeWeek");
+var freeWeekChampKey = [];
 
 async function callSummonerv4() {
     var accountName = document.getElementById("lol-username").value;
@@ -24,15 +25,41 @@ async function callSummonerv4() {
 
 btnGetAccount.addEventListener('click', callSummonerv4);
 
-function callChampionRotation() {
-    var APICallString = "https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY
+ function callChampionRotation() {
+    var APICallString = "https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY;
     
-    fetch(APICallString)
+    freeWeekChampKey = fetch(APICallString)
     .then(function(response){
         return response.json();
     })
     .then(function(data) {
-        console.log(data.freee);
+        return data.freeChampionIds;
+         //freeWeekChampKey = data.freeChampionIds;
+         //console.log(freeWeekChampKey);
     })
+
+
+    fetch("./assets/champion-data/champion.json")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data) {
+
+        
+
+        for (const champ in data.data){
+             console.log(champ)
+             console.log(data.data[champ].key);
+             console.log(freeWeekChampKey);
+            var x = data.data[champ].key;
+            if (freeWeekChampKey.includes(x)) {
+                console.log(champ);
+            }
+
+            }
+    })
+
 }
+
+
 btnGetFreeWeek.addEventListener('click', callChampionRotation);
